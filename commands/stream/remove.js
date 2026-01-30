c5933a4cbb1ecaa962ccf256c98186993a4dcc36
Mastinoo <dylan@fraiquin.eu>
2026-01-30 12:48:09 +0100

    Update remove.js

--- commands/stream/remove.js
+++ commands/stream/remove.js
@@ -1,1 +1,14 @@
+import { SlashCommandBuilder } from 'discord.js';
+import db from '../../database.js';
+
+export const data = new SlashCommandBuilder()
+  .setName('stream-remove')
+  .setDescription('Remove a tracked streamer')
+  .addUserOption(opt => opt.setName('discord').setDescription('Discord user linked to streamer').setRequired(true));
+
+export async function execute(interaction) {
+  const discordUser = interaction.options.getUser('discord');
+  const result = db.prepare("DELETE FROM streamers WHERE guildId=? AND discordUserId=?").run(interaction.guild.id, discordUser.id);
+  interaction.reply({ content: result.changes > 0 ? `✅ Removed streamer for ${discordUser.tag}` : '❌ No streamer found.', ephemeral: true });
+}
 

60b20d1b15c6c4c9667c4b0d0a6bb8e7a6b5117f
Mastinoo <dylan@fraiquin.eu>
2026-01-30 12:48:28 +0100

    Update list.js

--- commands/stream/list.js
+++ commands/stream/list.js
@@ -1,1 +1,18 @@
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import db from '../../database.js';
+
+export const data = new SlashCommandBuilder()
+  .setName('stream-list')
+  .setDescription('List all tracked streamers');
+
+export async function execute(interaction) {
+  const streamers = db.prepare("SELECT * FROM streamers WHERE guildId=?").all(interaction.guild.id);
+  if (!streamers.length) return interaction.reply({ content: 'No streamers tracked.', ephemeral: true });
+
+  const embed = new EmbedBuilder()
+    .setTitle('Tracked Streamers')
+    .setColor(0x9146FF)
+    .setDescription(streamers.map(s => `${s.platformUsername} (${s.platform}) ${s.isLive ? '🟢 LIVE' : ''}`).join('\n'));
+  interaction.reply({ embeds: [embed], ephemeral: true });
+}
 

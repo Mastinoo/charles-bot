90445b994efb67b5488d365f7ca7d35abdd3a94b
Mastinoo <dylan@fraiquin.eu>
2026-01-30 12:48:49 +0100

    Update setchannel.js

--- commands/stream/setchannel.js
+++ commands/stream/setchannel.js
@@ -1,1 +1,14 @@
+import { SlashCommandBuilder } from 'discord.js';
+import db from '../../database.js';
+
+export const data = new SlashCommandBuilder()
+  .setName('stream-setchannel')
+  .setDescription('Set the announcement channel')
+  .addChannelOption(opt => opt.setName('channel').setDescription('Text channel').setRequired(true));
+
+export async function execute(interaction) {
+  const channel = interaction.options.getChannel('channel');
+  db.prepare("UPDATE streamers SET announceChannelId=? WHERE guildId=?").run(channel.id, interaction.guild.id);
+  interaction.reply({ content: `✅ Announcement channel set to ${channel}`, ephemeral: true });
+}
 
